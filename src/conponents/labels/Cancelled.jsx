f5fdb874704c087dbc231390a2a5596049e62d29
DANNY <qnv164@gmail.com>
2021-04-02 15:13:57 +0700

    add labels

---
+++ src/conponents/labels/Cancelled.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Cancelled() {
+    return <span class="label label-lg label-canceled label-inline py-4">Đơn hủy</span>;
+}
---
+++ src/conponents/labels/Completed.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Completed() {
+    return <span class="label label-lg label-completed label-inline py-4">Hoàn thành</span>;
+}
