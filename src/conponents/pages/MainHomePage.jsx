8561f89874455eab387a5c2a0520f5d737ab8b1d
DANNY <qnv164@gmail.com>
2021-04-13 21:39:56 +0700

    add key value

--- src/conponents/pages/MainHomePage.jsx
+++ src/conponents/pages/MainHomePage.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import Header from "../common/Header";
-import Footer from "../common/Footer";
-import InProcessing from "../labels/InProcessing";
-import Picked from "../labels/Picked"
-import Completed from '../labels/Completed'
-import Cancelled from '../labels/Cancelled'
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+import InProcessing from '../labels/InProcessing';
+import Picked from '../labels/Picked';
+import Completed from '../labels/Completed';
+import Cancelled from '../labels/Cancelled';
 
 MainHomePage.propTypes = {
     datas: PropTypes.object,
@@ -21,8 +21,8 @@ function MainHomePage(props) {
     const { datas, ChangeOrderStatus } = props;
 
     function handleChangeStatus(data) {
-        if(ChangeOrderStatus){
-            ChangeOrderStatus(data)
+        if (ChangeOrderStatus) {
+            ChangeOrderStatus(data);
         }
     }
 
@@ -31,49 +31,31 @@ function MainHomePage(props) {
         if (datas) {
             Object.values(datas).map((values, index) => {
                 items.push(
-                    <div >
+                    <div>
                         <div className="d-flex align-items-start">
                             <span className="bullet bullet-bar bg-orange align-self-stretch" />
                             <div className="d-flex flex-column flex-grow-1 ml-4">
                                 <header className="card-title content">
-                                    <span key={values.id_post}>
-                                        {values.id_post}
-                                    </span>
-                                    <span key={values.thoi_gian}>
-                                        {values.thoi_gian}
-                                    </span>
+                                    <span key={index}>{values.id_post}</span>
+                                    <span key={index}>{values.thoi_gian}</span>
                                 </header>
                                 <section className="card-info content">
                                     <p>
-                                        <span
-                                            className="payment"
-                                            key={values.phi_giao}
-                                        >
+                                        <span className="payment" key={index}>
                                             {values.phi_giao} - Tiền mặt
                                         </span>
                                         <br />
-                                        <span key={values.ten_nguoi_gui}>
-                                            {values.ten_nguoi_gui}
-                                        </span>{" "}
-                                        -{" "}
-                                        <span key={values.sdt_nguoi_gui}>
-                                            {values.sdt_nguoi_gui}
-                                        </span>
+                                        <span key={index}>{values.ten_nguoi_gui}</span> - <span key={index}>{values.sdt_nguoi_gui}</span>
                                     </p>
-                                    <span className="delivery">
-                                        Giao hàng tới
-                                    </span>
+                                    <span className="delivery">Giao hàng tới</span>
                                     <div className="d-flex align-items-center justify-content-between">
-                                        <address
-                                            className="mb-0 pl-0 col-9"
-                                            key={values.noi_giao}
-                                        >
+                                        <address className="mb-0 pl-0 col-9" key={index}>
                                             {values.noi_giao}
                                         </address>
-                                        {values.status == "1" &&  <Picked />}
-                                        {values.status == "0" &&  <InProcessing />}
-                                        {values.status == "2" &&  <Completed />}
-                                        {values.status == "3" &&  <Cancelled />}
+                                        {values.status == '1' && <Picked />}
+                                        {values.status == '0' && <InProcessing />}
+                                        {values.status == '2' && <Completed />}
+                                        {values.status == '3' && <Cancelled />}
                                     </div>
                                 </section>
                             </div>
@@ -83,7 +65,7 @@ function MainHomePage(props) {
                 );
             });
         } else {
-            items.push("Chưa có đơn nào")
+            items.push('Chưa có đơn nào');
         }
         return items;
     }
@@ -96,37 +78,24 @@ function MainHomePage(props) {
                     <header className="card-header border-0">
                         <div className="card-title py-4">
                             <h3 className="card-label">
-                                <span className="d-block title">
-                                    Danh sách đơn
-                                </span>
-                                <span className="d-block text-time mt-2 font-size-sm">
-                                    trong 24 giờ
-                                </span>
+                                <span className="d-block title">Danh sách đơn</span>
+                                <span className="d-block text-time mt-2 font-size-sm">trong 24 giờ</span>
                             </h3>
                         </div>
                         <div className="card-toolbar">
                             <ul className="nav nav-pills">
                                 <li className="nav-item">
-                                    <a
-                                        href="#tab1"
-                                        className="nav-link py-2 px-4"
-                                    >
+                                    <a href="#tab1" className="nav-link py-2 px-4">
                                         <span className="nav-text">Tháng</span>
                                     </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a
-                                        href="#tab2"
-                                        className="nav-link py-2 px-4"
-                                    >
+                                    <a href="#tab2" className="nav-link py-2 px-4">
                                         <span className="nav-text">Tuần</span>
                                     </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a
-                                        href="#tab3"
-                                        className="nav-link py-2 px-4 active"
-                                    >
+                                    <a href="#tab3" className="nav-link py-2 px-4 active">
                                         <span className="nav-text">Ngày</span>
                                     </a>
                                 </li>
